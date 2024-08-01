e1f996f8faf9a198c212e2d61975d41f1be1c13e
pizzkun <80930842+pizzkun@users.noreply.github.com>
2024-08-01 22:01:01 +0700

    Update index.js

--- hbd/scripts/index.js
+++ hbd/scripts/index.js
@@ -10,7 +10,7 @@ function validateForm() {
   var password = document.getElementById("password").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
-  if (username === "indira" && password === "indira") {
+  if (username === "Indira" && password === "Indira") {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
